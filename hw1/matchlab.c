1fdf686cf452f2816dbb3114f82c03e026a4e9fd
Aaron <pabstaaron@gmail.com>
2017-01-11 07:26:24 -0700

    Added mode functions for hw1

--- hw1/matchlab.c
+++ hw1/matchlab.c
@@ -7,6 +7,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*
+ * Returns a reformated str if the input matches the pattern.
+ * Returns a string containing only a null terminator otherwise.
+ */
+char* aMode(char* str){
+  char currChar = str[0];
+  int i = 0;
+  short isMatch = 1; // Start by assuming string is a match
+
+  int charCounter = 0; // Used for counting how many times a char occurs
+  int region = 0; // 0 for m, 1 for v, 2 for numerical
+
+  // Loop through all the chars in the array
+  while(currChar != 0){ // Keep going until we hit a null terminator
+    if(region == 0){
+      if(i == 0 && currChar != m){ // Bail if not valid
+	isMatch = 0;
+	break;
+      }
+      
+    }
+    else if(region == 1){
+
+    }
+    else{
+
+    }
+
+    i = i+1;
+    currChar = str[i];
+  }
+}
+
+char* bMode(char* str){
+
+}
+
+char* cMode(char* str){
+
+}
+
 int main(int argc, char** argv){
   if(argc != 3 && argc != 4){
     printf("ERROR: Incorrect number of arguements!\n");
@@ -30,6 +71,8 @@ int main(int argc, char** argv){
       option = argv[1];
     else{
       printf("ERROR: Invalid argument!\n");
+      printf("%s\n", argv[1]);
+      printf("%s\n", argv[2]);
       return 1;
     }
     tSelect = 1;
